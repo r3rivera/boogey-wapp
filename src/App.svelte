37ddba88b54f87cbb71680445e67141774367fc2
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-19 20:26:49 -0500

    Added UUID package and explored loops

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,27 @@
 <script>
-	import PrimaryButton from './lib/PrimeBtn.svelte';
-	import SecondaryButton from './lib/SecondaryButton.svelte';
-	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
-	import FaAngleRight from 'svelte-icons/fa/FaAngleRight.svelte';
-	import FaAngleDoubleRight from 'svelte-icons/fa/FaAngleDoubleRight.svelte';
+	import Cards from './lib/Cards.svelte';
+	import { v4 as uuid } from 'uuid';
+
+	const bucketList = [
+		{ id: uuid(), title: 'House', desc: 'US House', completed: false },
+		{ id: uuid(), title: 'Condo', desc: 'Condo Unit in the Home Country', completed: false },
+		{ id: uuid(), title: 'Car', desc: 'SUV in US', completed: true },
+		{ id: uuid(), title: 'Dog', desc: 'Pet Dog - Labrador', completed: false },
+		{ id: uuid(), title: 'Home Business', desc: 'Business in Home', completed: false },
+		{ id: uuid(), title: 'Gun', desc: 'Real M4 Gun', completed: false },
+		{ id: uuid(), title: 'Garden', desc: 'Place to get away', completed: false },
+		{ id: uuid(), title: 'Money', desc: 'Money for happiness', completed: false }
+	];
 </script>
 
-<PrimaryButton
-	>Submit Button
-	<FaAngleRight slot="suffixIcon" />
-</PrimaryButton>
-<SecondaryButton let:isMouseHovered size="large">
-	<span slot="prefixName"><FaAngellist /></span>
-	<span>Cancel</span>
-	{#if isMouseHovered}
-		<FaAngleDoubleRight />
-	{:else}
-		<FaAngleRight />
-	{/if}
-</SecondaryButton>
+{#each bucketList as bucket (bucket.id)}
+	<Cards>
+		<div slot="cardHeader">{bucket.title}</div>
+		<div slot="cardContent">{bucket.desc}</div>
+		<div>{bucket.completed}</div>
+	</Cards>
+{/each}
+<Cards />
 
 <style lang="scss">
 </style>
---
+++ src/lib/Cards.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<div class="card-container">
+	<div class="card-container--header"><slot name="cardHeader" /></div>
+	<div class="card-container--content"><slot name="cardContent">Items</slot></div>
+</div>
+
+<style lang="scss">
+	.card-container {
+		align-content: center;
+
+		padding: 6px;
+		margin: 12px 6px;
+
+		border-radius: 6px;
+		border: 1px solid #fff;
+
+		&--header {
+			color: #f00;
+			padding: 6px;
+		}
+
+		&--content {
+			padding: 6px;
+		}
+	}
+</style>
