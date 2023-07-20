bea04f766336164fc84ddb2874d8e298adddca1b
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-19 19:27:46 -0500

    Play with props and modifiers

--- src/lib/PrimeBtn.svelte
+++ src/lib/PrimeBtn.svelte
@@ -1,7 +1,13 @@
 <script>
+	export let btnWidth = '150px;';
 </script>
 
-<button>
+<button
+	{...$$restProps}
+	on:click|once|trusted={(event) => alert(true)}
+	on:focus
+	style="width:{btnWidth}"
+>
 	{#if $$slots.prefixIcon}
 		<span class="pIcon"><slot name="prefixIcon" /></span>
 	{/if}
@@ -14,9 +20,9 @@
 <style lang="scss">
 	button {
 		display: flex;
+
 		align-items: center;
 		padding: 10px 15px;
-
 		border: none;
 		border-radius: 6px;
 
@@ -27,6 +33,11 @@
 		.sIcon {
 			width: 16px;
 			margin: 3px 6px;
+		}
+
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
 		}
 	}
 </style>
