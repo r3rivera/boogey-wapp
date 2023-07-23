b4c0f4d870b3aa9c89aa28bbf56f2f6ef18bb59d
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-22 21:54:00 -0500

    Added base login form with input binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Cards from './lib/Cards.svelte';
+	import Login from './lib/Login.svelte';
 	import { v4 as uuid } from 'uuid';
 
 	const bucketList = [
@@ -12,15 +13,38 @@
 		{ id: uuid(), title: 'Garden', desc: 'Place to get away', completed: false },
 		{ id: uuid(), title: 'Money', desc: 'Money for happiness', completed: false }
 	];
+
+	let inputField;
+	function handlerAddSubmit() {
+		alert(inputField);
+	}
 </script>
 
-{#each bucketList as { id, title, desc, completed } (id)}
+<Login />
+<br />
+<br />
+<form class="add-todo-form" on:submit|preventDefault={handlerAddSubmit}>
+	<!-- Option 1
+		<input
+		on:input={(e) => {
+			console.log(e.currentTarget.value);
+		}}
+	/> -->
+
+	<!-- Option 2 - Binding the input element, accessible by using value prop
+		<input bind:this={inputField} /> -->
+
+	<input bind:value={inputField} />
+	<button type="submit">Add</button>
+</form>
+
+<!-- {#each bucketList as { id, title, desc, completed } (id)}
 	<Cards isCloseable={true}>
 		<div slot="cardHeader">{title}</div>
 		<div slot="cardContent">{desc}</div>
 		<div>{completed}</div>
 	</Cards>
 {/each}
-
+ -->
 <style lang="scss">
 </style>
---
+++ src/lib/Login.svelte
@@ -0,0 +1,33 @@
+<script>
+	let emailField;
+	let passwordField;
+
+	function onHandleLogin() {
+		alert('Email is ' + emailField + ', Password id ' + passwordField);
+	}
+</script>
+
+<div class="login">
+	<div>
+		<form class="login--form" on:submit|preventDefault={onHandleLogin}>
+			<input bind:value={emailField} />
+			<input bind:value={passwordField} />
+			<button type="submit">Login</button>
+		</form>
+	</div>
+</div>
+
+<style lang="scss">
+	.login {
+		background-color: #fff;
+
+		&--form {
+			display: flexbox;
+			border-radius: 5px;
+			border: 1px solid #d1d1d1;
+
+			padding: 10px;
+			margin: 10px;
+		}
+	}
+</style>
