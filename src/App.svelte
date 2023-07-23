151545992b1f73acbfbc50ee1038b84fa9fb4fd6
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-22 22:30:22 -0500

    Added binding of components to sync-up between components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-	import Cards from './lib/Cards.svelte';
-	import Login from './lib/Login.svelte';
 	import { v4 as uuid } from 'uuid';
+	import TodoTemp from './lib/TodoTemp.svelte';
 
-	const bucketList = [
+	let bucketList = [
 		{ id: uuid(), title: 'House', desc: 'US House', completed: false },
 		{ id: uuid(), title: 'Condo', desc: 'Condo Unit in the Home Country', completed: false },
 		{ id: uuid(), title: 'Car', desc: 'SUV in US', completed: true },
@@ -14,37 +13,17 @@
 		{ id: uuid(), title: 'Money', desc: 'Money for happiness', completed: false }
 	];
 
-	let inputField;
-	function handlerAddSubmit() {
-		alert(inputField);
-	}
+	let itemLabel = 'items';
+	$: console.log(bucketList);
+
+	setTimeout(() => {
+		itemLabel = 'in Items';
+		console.log('Done timeout');
+	}, 3000);
 </script>
 
-<Login />
-<br />
-<br />
-<form class="add-todo-form" on:submit|preventDefault={handlerAddSubmit}>
-	<!-- Option 1
-		<input
-		on:input={(e) => {
-			console.log(e.currentTarget.value);
-		}}
-	/> -->
+<h3>{bucketList.length} {itemLabel}</h3>
+<TodoTemp bind:todoList={bucketList} />
 
-	<!-- Option 2 - Binding the input element, accessible by using value prop
-		<input bind:this={inputField} /> -->
-
-	<input bind:value={inputField} />
-	<button type="submit">Add</button>
-</form>
-
-<!-- {#each bucketList as { id, title, desc, completed } (id)}
-	<Cards isCloseable={true}>
-		<div slot="cardHeader">{title}</div>
-		<div slot="cardContent">{desc}</div>
-		<div>{completed}</div>
-	</Cards>
-{/each}
- -->
 <style lang="scss">
 </style>
---
+++ src/lib/TodoTemp.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { v4 as uuid } from 'uuid';
+
+	export let todoList;
+
+	let inputField;
+	function handlerAddSubmit() {
+		if (!inputField) return;
+
+		todoList = [
+			...todoList,
+			{
+				id: uuid(),
+				title: inputField,
+				desc: 'Something Added',
+				completed: false
+			}
+		];
+	}
+</script>
+
+<form on:submit|preventDefault={handlerAddSubmit}>
+	<input bind:value={inputField} />
+	<button type="submit">Submit</button>
+</form>
+
+{#if !todoList}
+	<div>Empty</div>
+{:else}
+	{#each todoList as todoItem}
+		<ul>{todoItem.title}</ul>
+	{/each}
+{/if}
